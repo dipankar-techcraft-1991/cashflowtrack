56c5ef961bc02a6c8b1f8044117c02e7a6c57488
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-03-23 20:55:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import ExpenseForm from "./component/ExpenseForm";
 import ExpenseTable from "./component/ExpenseTable";
-import expenseData from "../expenseData";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import "./App.css";
 
